7d04fdc0e3984d31117e716973473397e54c8ce3
ihsaneddin <ihsaneddin@gmail.com>
2025-08-06 14:32:40 +0700

    updates

--- lib/plugins/models/concerns/polymorphic_alternative.rb
+++ lib/plugins/models/concerns/polymorphic_alternative.rb
@@ -22,11 +22,11 @@ module Plugins
               belongs_to new_assoc.to_sym, foreign_key: rassoc.foreign_key, class_name: base_class.to_s, optional: true
               class_eval <<-CODE, __FILE__, __LINE__ + 1
                 def #{assoc}_instance
-                  relation = self.class.send("#{att}").find { |_key, val| val == send("#{rassoc.type}") }&.first
+                  relation = self.class.send('#{att}').find { |_key, val| val == '#{base_class.to_s}' }&.first
                   if respond_to?(relation)
                     send(relation)
                   else
-                    send("#{assoc}")
+                    send('#{assoc}')
                   end
                 end
               CODE
