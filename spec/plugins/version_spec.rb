0039e4c0b59e21ae3e4599f389204656c8430af5
ihsaneddin <ihsaneddin@gmail.com>
2026-05-16 04:06:46 +0700

    Bump plugins version to 1.1.0

--- spec/plugins/version_spec.rb
+++ spec/plugins/version_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../lib/plugins/version", __dir__)
 RSpec.describe Plugins do
   describe "VERSION" do
     it "defines the engine version" do
-      expect(described_class::VERSION).to eq("0.1.0")
+      expect(described_class::VERSION).to eq("1.1.0")
     end
   end
 end
